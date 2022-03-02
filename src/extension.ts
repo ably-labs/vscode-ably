fd17a820d3926c1a94f1e471c921b03e7887d1f2
Marc Duiker <marcduiker@users.noreply.github.com>
2022-03-02 15:46:59 +0000

    Add basic multi input (#12)

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { createAblyApp } from './command/CreateApp';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,10 +14,20 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-ably.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Ably for VSCode!');
+	let disposable = vscode.commands.registerCommand('vscode-ably.createApp', () => {
+		const options: { [key: string]: (context: vscode.ExtensionContext) => Promise<void> } = {
+			createAblyApp
+		};
+		const quickPick = vscode.window.createQuickPick();
+		quickPick.items = Object.keys(options).map(label => ({ label }));
+		quickPick.onDidChangeSelection(selection => {
+			if (selection[0]) {
+				options[selection[0].label](context)
+					.catch(console.error);
+			}
+		});
+		quickPick.onDidHide(() => quickPick.dispose());
+		quickPick.show();
 	});
 
 	context.subscriptions.push(disposable);
