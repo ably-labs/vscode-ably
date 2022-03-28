ea9176fe831b1e1b8989d9e14e7561cf6de47674
Marc Duiker <marcduiker@gmail.com>
2022-03-28 17:54:10 +0200

    Add fields for queues

--- src/appExplorer.ts
+++ src/appExplorer.ts
@@ -48,12 +48,11 @@ export class AblyAppProvider implements vscode.TreeDataProvider<AblyItem> {
 
         if(element.type === "app") {
             return Promise.resolve([
-                new AblyItem(`Status: ${element.data.status}`, element.internalId, element.data.status, "singleItem", vscode.TreeItemCollapsibleState.None, null, this.getStatusIcon(element.data.status)),
-                new AblyItem(`TLS only: ${element.data.tlsOnly}`, element.internalId, element.data.tlsOnly, "singleItem", vscode.TreeItemCollapsibleState.None, null, "lock"),
-                new AblyItem("API Keys", element.internalId, "keys" , "keyList", vscode.TreeItemCollapsibleState.Collapsed, undefined, "key"),
-                new AblyItem("Queues", element.internalId, "queues", "queueList", vscode.TreeItemCollapsibleState.Collapsed, undefined, "queue"),
-                new AblyItem("Rules", element.internalId, "rules", "ruleList", vscode.TreeItemCollapsibleState.Collapsed, undefined, "rule"),
-                new AblyItem("Namespaces", element.internalId, "namespaces", "namespaceList", vscode.TreeItemCollapsibleState.Collapsed, undefined, "namespace"),
+                new AblyItem(`Status: ${element.data.status}`, element.internalId, element.data.status, "singleItem", vscode.TreeItemCollapsibleState.None, undefined, this.getStatusIcon(element.data.status)),
+                new AblyItem(`TLS only: ${element.data.tlsOnly}`, element.internalId, element.data.tlsOnly, "singleItem", vscode.TreeItemCollapsibleState.None, undefined, "shield"),
+                new AblyItem("API Keys", element.internalId, "API Keys" , "keyList", vscode.TreeItemCollapsibleState.Collapsed, undefined, "key"),
+                new AblyItem("Queues", element.internalId, "Queues", "queueList", vscode.TreeItemCollapsibleState.Collapsed, undefined, "mail"),
+                new AblyItem("Integration Rules", element.internalId, "Integration Rules", "ruleList", vscode.TreeItemCollapsibleState.Collapsed, undefined, "symbol-event"),
             ]);
         }
 
@@ -66,8 +65,33 @@ export class AblyAppProvider implements vscode.TreeDataProvider<AblyItem> {
         if(element.type === "queueList"){
             const {data: queues} = await this.ax.get(`apps/${element.internalId}/queues`);
             const sortedQueues = queues.sort((a: any, b: any) => a.name.localeCompare(b.name));
-            return sortedQueues.map((queue: any) => new AblyItem(queue.name, queue.id, queue.name, "queue", vscode.TreeItemCollapsibleState.None, queue));
+            return sortedQueues.map((queue: any) => new AblyItem(queue.name, queue.id, queue.name, "queue", vscode.TreeItemCollapsibleState.Collapsed, queue, "mail"));
         }
+
+        if(element.type === "queue"){
+            const messagesItem = new AblyItem("Messages", element.internalId, "Messages", "messagesList", vscode.TreeItemCollapsibleState.Collapsed, element.data.messages, "mail");
+            const statsItem = new AblyItem("Stats", element.internalId, "Stats", "statsList", vscode.TreeItemCollapsibleState.Collapsed, element.data.stats, "pulse");
+            const regionItem = new AblyItem(`Region: ${element.data.region} `, element.internalId, element.data.region, "singleItem", vscode.TreeItemCollapsibleState.None, null, "globe");
+            const stateItem = new AblyItem(`State: ${element.data.state} `, element.internalId, element.data.state, "singleItem", vscode.TreeItemCollapsibleState.None, null, this.getStatusIcon(element.data.state));
+            const ttlItem = new AblyItem(`TTL: ${element.data.ttl} `, element.internalId, element.data.ttl, "singleItem", vscode.TreeItemCollapsibleState.None, null, "watch");
+            const maxLengthItem = new AblyItem(`Max length: ${element.data.maxLength} `, element.internalId, element.data.maxLength, "singleItem", vscode.TreeItemCollapsibleState.None, null, "arrow-both");
+            return [messagesItem, statsItem, regionItem, stateItem, ttlItem, maxLengthItem];
+        }
+
+        if(element.type === "messagesList"){
+            const readyItem = new AblyItem(`Ready: ${element.data.ready} `, element.internalId, element.data.ready, "singleItem", vscode.TreeItemCollapsibleState.None, null, "mail");
+            const unacknowledgedItem = new AblyItem(`Unacknowledged: ${element.data.unacknowledged} `, element.internalId, element.data.unacknowledged, "singleItem", vscode.TreeItemCollapsibleState.None, null, "mail");
+            const totalItem = new AblyItem(`Total: ${element.data.total} `, element.internalId, element.data.total, "singleItem", vscode.TreeItemCollapsibleState.None, null, "mail");
+            return [readyItem, unacknowledgedItem, totalItem];
+        }
+
+        if(element.type === "statsList"){
+            const publishRateItem = new AblyItem(`Publish rate: ${element.data.publishRate ?? '-'} `, element.internalId, element.data.publishRate, "singleItem", vscode.TreeItemCollapsibleState.None, null, "pulse");
+            const deliveryRateItem = new AblyItem(`Delivery rate: ${element.data.deliveryRate  ?? '-'} `, element.internalId, element.data.deliveryRate, "singleItem", vscode.TreeItemCollapsibleState.None, null, "pulse");
+            const acknowledgementRateItem = new AblyItem(`Acknowledgement rate: ${element.data.acknowledgementRate  ?? '-'} `, element.internalId, element.data.acknowledgementRate, "singleItem", vscode.TreeItemCollapsibleState.None, null, "pulse");
+            return [publishRateItem, deliveryRateItem, acknowledgementRateItem];
+        }
+
 
         if(element.type === "ruleList"){
             const {data: rules} = await this.ax.get(`apps/${element.internalId}/rules`);
@@ -75,18 +99,12 @@ export class AblyAppProvider implements vscode.TreeDataProvider<AblyItem> {
             return sortedRules.map((rule: any) => new AblyItem(`${rule.source.channelFilter}`, rule.id, rule.source.channelFilter, "rule", vscode.TreeItemCollapsibleState.None, rule));
         }
 
-        if(element.type === "namespaceList"){
-            const {data: namespaces} = await this.ax.get(`apps/${element.internalId}/namespaces`);
-            const sortedNameSpaces = namespaces.sort((a: any, b: any) => a.id.localeCompare(b.id));
-            return sortedNameSpaces.map((namespace: any) => new AblyItem(namespace.id, namespace.id, namespace.id, "namespace", vscode.TreeItemCollapsibleState.None, namespace));
-        }
-        
         if(element.type === "key"){
             const dateCreated = new Date(element.data.created).toUTCString();
             const createdItem = new AblyItem(`Created: ${dateCreated} ` , dateCreated, dateCreated, "singleItem", vscode.TreeItemCollapsibleState.None, null, "calendar");
             const dateModified = new Date(element.data.modified).toUTCString();
             const modifiedItem = new AblyItem(`Modified: ${dateModified} ` , dateModified, dateModified, "singleItem", vscode.TreeItemCollapsibleState.None, null, "calendar");
-            const resourceRestriction = Object.keys(element.data.capability).map((resource: string) => new AblyItem(`Resource restriction: ${resource}`, resource, resource, "singleItem", vscode.TreeItemCollapsibleState.None, null, "restriction"));
+            const resourceRestriction = Object.keys(element.data.capability).map((resource: string) => new AblyItem(`Resource restriction: ${resource}`, resource, resource, "singleItem", vscode.TreeItemCollapsibleState.None, null, "lock"));
             const capabilities = Object.keys(element.data.capability).map((resource: string) => new AblyItem("Capabilities", "Capabilities", "Capabilities", "keyCapChannel", vscode.TreeItemCollapsibleState.Collapsed, element.data.capability[resource], "checklist"));
             return capabilities.concat(resourceRestriction, createdItem, modifiedItem);
         }
@@ -147,7 +165,7 @@ export class AblyAppProvider implements vscode.TreeDataProvider<AblyItem> {
 }
 
 
-type AblyItemType = "app" | "key" | "queue" | "rule" | "namespace" | "keyList" | "queueList" | "ruleList" | "namespaceList" | "singleItem" | "keyCapChannel" | "keyCapability";
+type AblyItemType = "app" | "key" | "queue" | "rule" | "namespace" | "keyList" | "queueList" | "ruleList" | "messagesList"| "namespaceList" | "singleItem" | "statsList" | "keyCapChannel" | "keyCapability";
 
 
 export class AblyItem extends vscode.TreeItem {
