41f45ff08937497c09e2eab58445bb5b540c9f11
Marc Duiker <marcduiker@gmail.com>
2022-04-06 15:37:08 +0200

    Add telemetry & remove remoke since it's not yet working

--- src/extension.ts
+++ src/extension.ts
@@ -2,18 +2,20 @@ import * as vscode from 'vscode';
 import { AblyAppProvider } from './appExplorer';
 import { createAblyApp } from './command/CreateApp';
 import { AblyControlApi } from './AblyControlApi';
+import { TelemetryApi, EventName } from './TelemetryApi';
 
 export function activate(context: vscode.ExtensionContext) {
 	const config = vscode.workspace.getConfiguration("ably");
 	const extensionVersion = context.extension.packageJSON.version;
+	const telemetryProvider = new TelemetryApi(extensionVersion);
+	telemetryProvider.postEvent(EventName.Activated);
 	const ablyControlApi = new AblyControlApi(config, extensionVersion);
-	const ablyAppProvider = new AblyAppProvider(config, ablyControlApi);
+	const ablyAppProvider = new AblyAppProvider(config, ablyControlApi, telemetryProvider);
 	vscode.window.registerTreeDataProvider('ablyAppExplorer', ablyAppProvider);
 	vscode.commands.registerCommand("ably.copyToClipboard", ablyAppProvider.handleCopy);
 	vscode.commands.registerCommand("ably.refresh", ablyAppProvider.refresh);
-	vscode.commands.registerCommand("ably.revokeKey", ablyAppProvider.handleRevokeKey);
 	let disposable = vscode.commands.registerCommand('ably.createApp', () => {
-		createAblyApp(context, ablyControlApi)
+		createAblyApp(context, ablyControlApi, telemetryProvider)
 			.catch(console.error);
 	});
 
